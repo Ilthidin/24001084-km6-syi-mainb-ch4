ad9da03b556a85be508db2bd469da0624a20ee90
Muhammad Syihab <syihab.demak@gmail.com>
2024-03-28 16:29:20 +0700

    Refactoring data

--- app/src/main/java/com/example/mainb/data/datasource/catalog/CatalogDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mainb.data.datasource.catalog
-
-import com.example.mainb.data.model.Catalog
-
-interface CatalogDataSource {
-    fun getCatalogs(): List<Catalog>
-}
--- app/src/main/java/com/example/mainb/data/datasource/product/DummyProductDataSource.kt
+++ app/src/main/java/com/example/mainb/data/datasource/product/DummyProductDataSource.kt
@@ -1,53 +1,53 @@
-package com.example.mainb.data.datasource.catalog
+package com.example.mainb.data.datasource.product
 
-import com.example.mainb.data.model.Catalog
+import com.example.mainb.data.model.Product
 
-class DummyCatalogDataSource : CatalogDataSource {
-    override fun getCatalogs(): List<Catalog> {
+class DummyProductDataSource : ProductDataSource {
+    override fun getProducts(): List<Product> {
         return listOf(
-            Catalog(
+            Product(
                 name = "Nasi Goreng",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_fried_rice.jpg?raw=true",
                 price = 30.0,
                 desc = "Nasi yang diberi bumbu dan digoreng bersamaan dengan daging asli",
             ),
-            Catalog(
+            Product(
                 name = "Burger",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_burger.jpg?raw=true",
                 price = 30.0,
                 desc = "Makanan cepat saji yang terdiri dari roti, daging, sayur, dan berbagai macam isian lainnya",
             ),
-            Catalog(
+            Product(
                 name = "Spaghetti",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_spaghetti.jpg?raw=true",
                 price = 30.0,
                 desc = "Mie berbentuk pasta asal italia yang memiliki berbagai macam topping dan saus khas italia",
             ),
-            Catalog(
+            Product(
                 name = "Bakso",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_meatball.jpg?raw=true",
                 price = 30.0,
                 desc = "Daging giling yang berbentuk bola dipadu dengan mie dan daging lain seta ditambahkan dengan kuah kaldu asli",
             ),
-            Catalog(
+            Product(
                 name = "Es Teh",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_ice_tea.jpg?raw=true",
                 price = 30.0,
                 desc = "Minumal seduhan teh ditambah dengan es memberi rasa segar",
             ),
-            Catalog(
+            Product(
                 name = "Salad",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_salad.jpg?raw=true",
                 price = 30.0,
                 desc = "Berbagai macam sayuran hijau segar yang dicampur memiliki banyak manfaat bagi kesehatan tubuh",
             ),
-            Catalog(
+            Product(
                 name = "Ramen",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_ramen.jpg?raw=true",
                 price = 30.0,
                 desc = "Mie asal jepang yang diberikan berbagai macam topping serta kaldu alami sehingga menciptakan rasa yang unik",
             ),
-            Catalog(
+            Product(
                 name = "Sushi Roll",
                 imgUrl = "https://github.com/Ilthidin/food_assets/blob/main/Catalog/img_sushi.jpg?raw=true",
                 price = 30.0,
